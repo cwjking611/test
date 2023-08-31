fd57e002f0d877f0598802b8aa26e23a9d0f05c4
cwjking611 <61800609+cwjking611@users.noreply.github.com>
2023-08-31 16:20:52 +0000

    update

--- generate_pna.py
+++ generate_pna.py
@@ -9,4 +9,38 @@
 #
 # Note: This script requires the Azure CLI to be installed and logged in to an Azure subscription.
 #
-What do I code next?
+az login
+#   Get list of all resource types
+resource_types=$(az provider list --query [].namespace --output tsv)
+#   Loop through each resource type and get publicNetworkAccess property
+for resource_type in $resource_types
+do
+    public_network_access=$(az provider show --namespace $resource_type --query "resourceTypes[?resourceType=='*'].properties.publicNetworkAccess" --output tsv)
+    #   If publicNetworkAccess property is not null, then add resource type to list of resources that support publicNetworkAccess
+    if [ "$public_network_access" != "None" ]
+    then
+        echo $resource_type
+    fi
+done
+```
+
+## Create policy definitions
+
+The following script creates a policy definition in Azure for each resource type in the pna_resources.json file.
+    
+    ```bash
+    #!/bin/bash
+    #
+    # Path: create_pna_policy_definitions.sh
+    # Create a policy definition in Azure for each resource type in the pna_resources.json file.
+    #       
+    # Usage: ./create_pna_policy_definitions.sh
+    #                           
+    # Example:  ./create_pna_policy_definitions.sh
+    #
+    # Note: This script requires the Azure CLI to be installed and logged in to an Azure subscription.
+    #
+    # Get list of resource types that support publicNetworkAccess
+    resource_types=$(cat pna_resources.json | jq -r '.[]')
+    # Loop through each resource type and create policy definition
+    for resource_type in $resource_types
